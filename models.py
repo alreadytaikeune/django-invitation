3059b6d1f3d5ef542214b2039fc2e22390fca47b
giliam <matthieu.denoux@gmail.com>
2015-09-08 19:52:42 +0200

    Fixes a mistype on Invitation model, adds some methods to the manager and adds a few comments.

--- models.py
+++ models.py
@@ -33,23 +33,39 @@ EVENT_MODEL = getattr(settings, 'EVENT_MODEL', '')
 
 
 
-class InviationManager(models.Manager):
+class InvitationManager(models.Manager):
 	
 	def invitations(self, user):
+		"""Returns the invitations context created by a specific user."""
 		return InvitationContext.objects.filter(inviter=user)
 	
 	def invited_to(self, user):
+		"""Returns the invitations context addressed to a specific user."""
 		return InvitationContext.objects.filter(invitation_set__invitee = user)
 	
 	def invitations_sent(self, event):
+		"""Returns the invitations related to an event."""
 		return Invitations.objects.filter(context__event = event)
+
+	def invitations_sent_by(self, user):
+		"""Returns the invitations created by an inviter."""
+		return Invitations.objects.filter(context__inviter = user)
 	
+	def create_invite(self, inviter, invitee, event):
+		"""
+			Creates an invitation and its context for an event 
+			from an inviter to the invitee.
+		"""
+		return invite(invitee,create_context(inviter, event))
+
 	def create_context(self, user, event):
+		"""Creates an invitation context for an event from an inviter."""
 		new_context = InvitationContext(inviter = user, event=event)
 		new_context.save()
 		return new_context
 	
 	def invite(self, user, context):
+		"""Creates an invitation in a context for an invitee."""
 		new_invite = Invitation(context=context, invitee=user)
 		new_invite.save()
 		invitation_sent.send(self, context = context, to_user = user)
@@ -83,6 +99,8 @@ class InvitationContext(models.Model):
 	def __str__(self):
 		return "User {0} is inviting at {1}".format(self.inviter_id, self.event_id)
 	
+	def __unicode__(self):
+		return u'User {0} is inviting at {1}'.format(self.inviter_id, self.event_id)
 
 
 
@@ -113,7 +131,6 @@ class Invitation(models.Model):
 		self.save()
 		invitation_rejected.send(sender = self, by_user=invitee, event=context.event)
 		return True
-		
 	
 	def mark_viewed():
 		self.viewed = timezone.now()
@@ -122,7 +139,7 @@ class Invitation(models.Model):
 		return True
 		
 	def __str__(self):
-		return u'invitation for event %s' % self.context.event.name
+		return 'invitation for event %s' % self.context.event.name
 		
 	def __unicode__(self):
 		return u'invitation for event %s' % self.context.event.name
