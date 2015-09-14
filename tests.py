0b5315d0fd00b51ef0a50c0565a3f463e0f68cfc
giliam <matthieu.denoux@gmail.com>
2015-09-14 21:27:08 +0200

    Adds tests for signals.

--- tests.py
+++ tests.py
@@ -54,42 +54,75 @@ class InvitationTest(TestCase):
         self.invitation_notification_day.save()
         self.invitation_notification_week.save()
 
+        self.signal_by_user = None
+        self.signal_event = None
+        self.signal_listened = False
+
 
     def test_accept_invitation(self):
         """
             Tests the accept method on InvitationModel.
-            ..todo::tests the signals
         """
-        # Default value of accepted is none
+        def invitation_accepted_listener(sender, by_user, event, **kwargs):
+            self.signal_by_user = by_user
+            self.signal_event = event
+        
+        # Connects to the signal
+        invitation_accepted.connect(invitation_accepted_listener, sender=self.invitation)
+        
+        # Default value of accepted is none then accepts it
         self.assertIsNone(self.invitation.accepted)
         self.assertTrue(self.invitation.accept())
+
         # The accept action updated it.
         self.assertIsNotNone(self.invitation.accepted)
+
+        # Tests that the signal has been sent and received.
+        self.assertEqual(self.invitee, self.signal_by_user)
+        self.assertEqual(self.event, self.signal_event)
 
 
     def test_reject_invitation(self):
         """
             Tests the reject method on InvitationModel.
-            ..todo::tests the signals
         """
-        # Default value of rejected is none
+        def invitation_rejected_listener(sender, by_user, event, **kwargs):
+            self.signal_by_user = by_user
+            self.signal_event = event
+        
+        # Connects to the signal
+        invitation_rejected.connect(invitation_rejected_listener, sender=self.invitation)
+
+        # Default value of rejected is none then rejects it
         self.assertIsNone(self.invitation.rejected)
         self.assertTrue(self.invitation.reject())
+
         # The reject action updated it.
         self.assertIsNotNone(self.invitation.rejected)
 
+        # Tests that the signal has been sent and received.
+        self.assertEqual(self.invitee, self.signal_by_user)
+        self.assertEqual(self.event, self.signal_event)
 
     def test_mark_viewed_invitation(self):
         """
             Tests the mark_viewed method on InvitationModel.
-            ..todo::tests the signals
         """
+        def invitation_viewed_listener(sender, **kwargs):
+            self.signal_listened = True
+
+        # Connects to the signal
+        invitation_viewed.connect(invitation_viewed_listener, sender=self.invitation)
+        
         # Default value of viewed is none
         self.assertIsNone(self.invitation.viewed)
         self.assertTrue(self.invitation.mark_viewed())
+
         # The view action updated it.
         self.assertIsNotNone(self.invitation.viewed)
-
+        
+        # Tests that the signal has been sent and received.
+        self.assertTrue(self.signal_listened)
 
     def test_find_invitations_to_notify(self):
         invitations = find_invitations_to_notify()
